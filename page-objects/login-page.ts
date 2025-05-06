f0777252799d4d67ad27ec743a6ce78987b67387
monmanao <nekoemon@gmail.com>
2025-05-06 11:04:20 +0700

    login
    pom

---
+++ page-objects/login-page.ts
@@ -0,0 +1,35 @@
+import { expect, Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async goto() {
+    await this.page.goto('https://www.saucedemo.com/');
+
+
+    // await this.page.waitForSelector('#onetrust-accept-btn-handler', { state: 'visible' });
+    // await this.page.click('#onetrust-accept-btn-handler');
+
+    // // Verify the text "เข้าสู่ระบบ" is visible
+    // await this.page.waitForSelector('text=เข้าสู่ระบบ');
+  }
+
+//   async goToLoginPage() {
+//     await this.page.click('#header-login-link');
+  
+//     await expect(this.page).toHaveURL('https://www.saucedemo.com/');
+//   }
+
+  async login(username: string, password: string) {
+    await this.page.fill('#user-name', username);
+    await this.page.fill('#password', password);
+    await this.page.click('#login-button');
+
+  }
+
+  async expectToBeOnDashboard() {
+
+    await expect(this.page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    
+  }
+}
